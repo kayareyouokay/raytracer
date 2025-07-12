ebc9e59c86a58e5849c5c604b9427cbe72ec9db2
Krish Bhardwaj <Krishbhardwaj0619@gmail.com>
2025-07-12 23:51:02 +0530

    add explicit type parameters to the gen() calls

--- src/vec3.rs
+++ src/vec3.rs
@@ -17,9 +17,9 @@ impl Vec3 {
     pub fn random() -> Self {
         let mut rng = rand::thread_rng();
         Self {
-            x: rng.gen(),
-            y: rng.gen(),
-            z: rng.gen(),
+            x: rng.gen::<f64>(),
+            y: rng.gen::<f64>(),
+            z: rng.gen::<f64>(),
         }
     }
 
